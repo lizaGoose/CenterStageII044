e6eee8a18d5e0e81f966d6918fab53e0ae1a24ca
Ivan <vanja221206@gmail.com>
2024-02-21 18:19:10 +0300

    13.02.2024.Liza

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
@@ -15,30 +15,35 @@ public class MyClass {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(38.945723632307676, 38.156578905846143, Math.toRadians(180), Math.toRadians(180), 6.93)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-34, 56, 0))
-                                .splineToLinearHeading(new Pose2d(-5, -5, Math.toRadians(0)), Math.toRadians(0))
-                                .splineToLinearHeading(new Pose2d(-10, -38, Math.toRadians(0)), Math.toRadians(0))
+                        drive.trajectorySequenceBuilder(new Pose2d(-38, 61.5, 0))
+                                //.lineToLinearHeading(new Pose2d(-44, 21, Math.toRadians(0))) // senter
+//                                .lineToLinearHeading(new Pose2d(-33, 32, 0)) // left
+                                .lineToLinearHeading(new Pose2d(-56.4, 32, 0)) // right
                                 .waitSeconds(0.3)
-
-                                .splineToLinearHeading(new Pose2d(-23.5, -52, 0), Math.toRadians(80))
+//                                .lineToLinearHeading(new Pose2d(-45, 32, 0))
+//                                 .splineToLinearHeading(new Pose2d(-60, 12, 0), Math.toRadians(-90)) // left, senter
+                                 .lineToLinearHeading(new Pose2d(-60, 12, 0))
                                 .waitSeconds(0.9)
 
                                 .waitSeconds(0.2)
 
 
                                 //.splineToConstantHeading(new Vector2d(48, -50), 0)
-                                .splineToLinearHeading(new Pose2d(88.7, -23.5, 0), Math.toRadians(60))
+                                .lineToLinearHeading(new Pose2d(36, 12, 0))
+//                                .lineToLinearHeading(new Pose2d(49, 34, 0))
+                                //.splineToLinearHeading(new Pose2d(49, 41, 0), Math.toRadians(0)) // left
+                                .splineToLinearHeading(new Pose2d(49, 30, 0), Math.toRadians(0)) // right
                                 .waitSeconds(0.5)
-                                .splineToLinearHeading(new Pose2d(70, -49.5, 0), Math.toRadians(180))
-                                .splineToLinearHeading(new Pose2d(0, -49.5, 0), Math.toRadians(180))
+                                .lineToLinearHeading(new Pose2d(36, 12, 0))
+                                .lineToLinearHeading(new Pose2d(-50, 12, 0))
+                                .splineToLinearHeading(new Pose2d(-60, 12, 0), Math.toRadians(90))
                                 .waitSeconds(0.6)
+                                .lineToLinearHeading(new Pose2d(36, 12, 0))
+                                .lineToLinearHeading(new Pose2d(49, 34, 0))
+                                .waitSeconds(0.5)
 
+                                .lineToLinearHeading(new Pose2d(42, 14, 0))
 
-
-                                //splineToConstantHeading(new Vector2d(48, -50), 0)
-
-                                .splineToLinearHeading(new Pose2d(88, -28.5, 0), Math.toRadians(60))
-                                .waitSeconds(0.3)
 
                                 .build()
                 );
