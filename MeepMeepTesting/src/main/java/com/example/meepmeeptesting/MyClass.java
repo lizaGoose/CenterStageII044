32094500a30c9d196bf696c0f97e52f4218860c6
susl2 <susl2007@yandex.ru>
2024-02-23 15:27:02 +0300

    Trajectorys testing for left positions.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
@@ -15,23 +15,23 @@ public class MyClass {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(38.945723632307676, 38.156578905846143, Math.toRadians(180), Math.toRadians(180), 6.93)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-38, -61.5, 0))
-//                                .lineToLinearHeading(new Pose2d(-44, -27, Math.toRadians(0))) // senter
-                                .lineToLinearHeading(new Pose2d(-33, -32, 0)) // left
+                        drive.trajectorySequenceBuilder(new Pose2d(12, 61.5, 0))
+//                                .lineToLinearHeading(new Pose2d(-44, -27, Math.toRa dians(0))) // senter
+                                .lineToLinearHeading(new Pose2d(11, 35, 0)) // left
                                  //.lineToLinearHeading(new Pose2d(-56.4, -32, 0)) // right
                                 .waitSeconds(0.3)
 //                                .lineToLinearHeading(new Pose2d(-45, -32, 0))
 //                                 .splineToLinearHeading(new Pose2d(-60, -12, 0), Math.toRadians(90)) // left, senter
-                                 .lineToLinearHeading(new Pose2d(-60, -12, 0))
+                                 //.lineToLinearHeading(new Pose2d(-60, -12, 0))
                                 .waitSeconds(0.9)
 
                                 .waitSeconds(0.2)
 
 
                                 //.splineToConstantHeading(new Vector2d(48, -50), 0)
-                                .lineToLinearHeading(new Pose2d(36, -12, 0))
+                                //.lineToLinearHeading(new Pose2d(36, 12, 0))
 //                                .lineToLinearHeading(new Pose2d(49, -34, 0))
-                                .splineToLinearHeading(new Pose2d(49, -41, 0), Math.toRadians(0)) // left
+                                .splineToLinearHeading(new Pose2d(49, 36, 0), Math.toRadians(0)) // left
                                 //.splineToLinearHeading(new Pose2d(49, -30, 0), Math.toRadians(0)) // right
                                 .waitSeconds(0.5)
                                 .lineToLinearHeading(new Pose2d(36, -12, 0))
